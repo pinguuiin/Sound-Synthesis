272def2db591466bda2fb8f050904cf0990f105f
ykadosh <ykadosh@student.hive.fi>
2025-08-29 22:12:31 +0300

    refines comments detailing sequencer's objectives

--- sequencer.c
+++ sequencer.c
@@ -1,9 +1,14 @@
 #include <sequencer.h>
 
 // WARN: For each and every note:
-// 		1. Determine the "attack" moment and the "release / cut" moment.
-// 		2. Send those two to the sound-machine part.
-t_info	orchestrate()
+// 		1.	Determine the "attack" moment and the "release / cut" moment.
+// 			Note that the moment of release should be a little bit BEFORE the
+// 			next note.
+// 		2.	Send those two to the sound-machine part
+// 			OR:
+// 			send the note itself, including the information about the 'attack'
+// 			and 'release'?
+t_note	orchestrate(t_info info)
 {
 
 
