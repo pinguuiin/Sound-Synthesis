2eb972feeda528a6c1008217f99bbac00fc7e1a6
ykadosh <ykadosh@student.hive.fi>
2025-08-29 21:55:39 +0300

    only adds some small comments regarding what to send to the sampler generator

--- sequencer.c
+++ sequencer.c
@@ -1,5 +1,8 @@
 #include <sequencer.h>
 
+// WARN: For each and every note:
+// 		1. Determine the "attack" moment and the "release / cut" moment.
+// 		2. Send those two to the sound-machine part.
 t_info	orchestrate()
 {
 
