0dd215533586f09ef95220f44e92e9df38a8d2ee
pinguuiin <yuping199407@hotmail.com>
2025-08-31 18:01:21 +0300

    Fix switch case falling through

--- processor.c
+++ processor.c
@@ -7,18 +7,25 @@ static void	pitch_to_frequency(t_note *note)
 	switch (note->pitch) {
 		case 'c':
 			n_semi = -9;
+			break ;
 		case 'd':
 			n_semi = -7;
+			break ;
 		case 'e':
 			n_semi = -5;
+			break ;
 		case 'f':
 			n_semi = -4;
+			break ;
 		case 'g':
 			n_semi = -2;
+			break ;
 		case 'a':
 			n_semi = 0;
+			break ;
 		case 'b':
 			n_semi = 2;
+			break ;
 	}
 	if (note->alteration == '#')
 		n_semi += 1;
